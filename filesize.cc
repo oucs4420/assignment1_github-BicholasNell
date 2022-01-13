9845302366d57c829e324b7bf8ce6c5f7737e5aa
Nicholas Bell <nb262619@ohio.edu>
2022-01-13 15:42:50 -0500

    first commit

--- filesize.cc
+++ filesize.cc
@@ -10,9 +10,20 @@ using namespace std;
 
 int main( int argc, char* argv[] )
 {
+    ifstream myfile;
+    string tmp;
     // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+    for (int arg = 1; arg < argc; ++arg){ //start at 1 to get file names
+            //std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+        int lineCount = 0; // initialize count at 0
+        myfile.open(argv[arg]); //open file
+        while(getline(myfile, tmp)){ //until end of file
+            lineCount++;
+        }
+        cout << argv[arg] << ": " << lineCount << endl; // print out to console
+    }
+
+    
 
     exit(0); // this means that the program executed correctly!
 }
